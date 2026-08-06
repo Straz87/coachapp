f14d3ed5d1a5983a5b112527126889fa59d6d6f6
Straz87 <MIMMOSTRAZ1987@HOTMAIL.IT>
2026-08-06 23:29:06 +0200

    Fix missing JSX closing tags in AMRAP input

--- coachapp/src/components/AllenamentoGiorno.tsx
+++ coachapp/src/components/AllenamentoGiorno.tsx
@@ -492,6 +492,7 @@ export default function AllenamentoGiorno({
                                             }
                                           />
                                         </div>
+                                        </div>
                                       </div>
                                     );
                                   }
