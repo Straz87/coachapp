2b6f639b15a5485057c96832a01516bc49abfd1d
Straz87 <MIMMOSTRAZ1987@HOTMAIL.IT>
2026-08-16 01:41:08 +0200

    Implement redirect for '/vai' path
    Added a redirect from '/vai' to a specific destination.

--- coachapp/next.config.js
+++ coachapp/next.config.js
@@ -6,7 +6,16 @@ const nextConfig = {
         protocol: "https",
         hostname: "**.supabase.co",
       },
-    ],
+      ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/vai",
+        destination: "/iscriviti/b341c464-50d6-4ba5-811e-f6652390fbdb/32c8454a-2d34-448e-8300-411133d043c7",
+        permanent: false,
+      },
+      ];
   },
 };
 
