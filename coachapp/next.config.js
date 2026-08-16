f06fd544dc1941c430171a1e3eadf201fd0508be
Straz87 <MIMMOSTRAZ1987@HOTMAIL.IT>
2026-08-16 02:04:45 +0200

    Remove redirects from next.config.js
    Removed redirects configuration from Next.js config.

--- coachapp/next.config.js
+++ coachapp/next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
       },
       ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/vai",
-        destination: "/iscriviti/b341c464-50d6-4ba5-811e-f6652390fbdb/32c8454a-2d34-448e-8300-411133d043c7",
-        permanent: false,
-      },
-      ];
-  },
 };
 
 module.exports = nextConfig;
